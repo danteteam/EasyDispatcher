170019b2f4c5a7fd95301e0b5b0dc3c9663eae44
Ivan Brazhnikov <samsungpc239@gmail.com>
2015-09-27 01:47:24 +0300

    Initial Commit

---
+++ EasyDispatcher/EasyDispatcher.h
@@ -0,0 +1,19 @@
+//
+//  EasyDispatcher.h
+//  EasyDispatcher
+//
+//  Created by Ivan Brazhnikov on 27/09/15.
+//  Copyright © 2015 dantelab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyDispatcher.
+FOUNDATION_EXPORT double EasyDispatcherVersionNumber;
+
+//! Project version string for EasyDispatcher.
+FOUNDATION_EXPORT const unsigned char EasyDispatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyDispatcher/PublicHeader.h>
+
+
